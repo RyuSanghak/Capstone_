9869a45e1a9cfda8b5d04be62fe09741c9d36082
Seokhyun Hong <seokhyunh320@gmail.com>
2024-11-13 15:00:06 -0500

    11/13

--- Capstone_/Algorithm/Dijkstra.swift
+++ Capstone_/Algorithm/Dijkstra.swift
@@ -63,10 +63,9 @@ func printCost(for path: [GKGraphNode]) {
 func findPath(buildingName: String, start: String, end: String) {
     
     var nodeList: [MyNode] = []
-<<<<<<< HEAD
-=======
+
     pathList.removeAll()
->>>>>>> sryu
+
     
 
     //빌딩별로 데이터 셋업
